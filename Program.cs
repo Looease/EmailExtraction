fee2eb72c7400c4cc2cd43005fc2a0591a5844a0
Louise <louise.gilligan@zoopla.co.uk>
2020-07-08 16:10:13 +0100

    Regex version

--- Program.cs
+++ Program.cs
@@ -1,26 +1,36 @@
-using System;
-using Systen.IO;
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace DemoEmailExtraction{
     class Program
     {
         static void Main(string[] args)
         {
-            var content = FieldAccessException.ReadAllText("sample.txt");
+            var content = File.ReadAllText("sample.txt");
+            var regex = new Regex(@"@softwire\.com");
 
-            var searchTerm = "@softwire.com"
-
-            var numberOfSoftwireEmails = 0;
-
-            for(var i = 0; i < content.Length - searchTerm.Length; i++)
-            {
-                var substring = content.Substring(i,searchTerm.Length);
-                if (substring == searchTerm){
-                    numberOfSoftwireEmails++;
-                }
-            }
-            Console.WriteLine(numberOfSoftwireEmails);
-        }
+            var results = regex.Matches(content);
+            Console.WriteLine(results.Count);
+        }       
     }
 
 }
+
+ // The below example is correct but long winded and not using Regex
+        // {
+        //     var content = File.ReadAllText("sample.txt");
+
+        //     var searchTerm = "@softwire.com";
+
+        //     var numberOfSoftwireEmails = 0;
+
+        //     for(var i = 0; i < content.Length - searchTerm.Length; i++)
+        //     {
+        //         var substring = content.Substring(i,searchTerm.Length);
+        //         if (substring == searchTerm){
+        //             numberOfSoftwireEmails++;
+        //         }
+        //     }
+        //     Console.WriteLine(numberOfSoftwireEmails);
+        // }
